db6f185f6fcfb1b4af71d71a59f6fe6fd515392a
Luan Wallace <w.luan@yahoo.com>
2022-06-04 12:40:44 -0300

    Foi criado estado para os input e criado um botão

--- modulo1/whatslab/src/App.js
+++ modulo1/whatslab/src/App.js
@@ -1,10 +1,50 @@
-
+import React, { useState } from 'react';
+import styled from 'styled-components'
 import './App.css';
 
+
 function App() {
+  // state do input
+  const [valueInputMessage, setvalueInputMessage] = useState('Message')
+  const [valueInputUser, setvalueInputUser] = useState('User')
+
+
+  // onChage USER and MESSAGE
+  const onChangeInputUserValue = (element) => {
+
+    setvalueInputUser(element.target.value)
+  }
+
+  const onChangeInputMessageValue = (element) => {
+    
+    setvalueInputMessage(element.target.value)
+  }
+
+  // onClick Button
+  const onClickButton = () => {
+
+    
+  
+    setvalueInputMessage("")
+    setvalueInputUser("")
+  }
+
+// #####    HTML    #####
   return (
     <div className="App">
-      
+      <div className='container'>
+        <input
+          value={valueInputUser}
+          onChange={onChangeInputUserValue}
+        />
+        <input
+          value={valueInputMessage}
+          onChange={onChangeInputMessageValue}
+        />
+        <button onClick={onClickButton}>
+          Enviar
+        </button>
+      </div>
     </div>
   );
 }
