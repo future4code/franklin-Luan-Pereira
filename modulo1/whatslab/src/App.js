dbbe10e4af9df7cbafe7416a78c69a71d1e06dc4
Luan Wallace <w.luan@yahoo.com>
2022-06-05 02:41:59 -0300

    Parte funcional inteiramente funcionando (falta css)

--- modulo1/whatslab/src/App.js
+++ modulo1/whatslab/src/App.js
@@ -5,8 +5,21 @@ import './App.css';
 
 function App() {
   // state do input
-  const [valueInputMessage, setvalueInputMessage] = useState('Message')
-  const [valueInputUser, setvalueInputUser] = useState('User')
+  const [valueInputMessage, setvalueInputMessage] = useState('')
+  const [valueInputUser, setvalueInputUser] = useState('')
+
+  const [onScreenMessage, setonScreenMessage] = useState(
+  [ 
+    {
+        // User: "",
+        // Message: "",
+    },                                          // That's an Array of Objects
+    {
+        // User: "",
+        // Message: "",
+    } 
+  ]
+);
 
 
   // onChage USER and MESSAGE
@@ -20,28 +33,53 @@ function App() {
     setvalueInputMessage(element.target.value)
   }
 
-  // onClick Button
-  const onClickButton = () => {
+       // onClick Button
+  const onClickSendMessage = () => {
+    const newMessage = {
+      user: valueInputUser + ":",
+      message: valueInputMessage,
+    };
 
-    
-  
-    setvalueInputMessage("")
-    setvalueInputUser("")
+       // We have to copy the array to avoid bug
+    const messageCopy = [...onScreenMessage,newMessage];
+
+    setonScreenMessage(messageCopy);
+      
+      // Clear Input
+    setvalueInputUser("");
+    setvalueInputMessage("");
   }
+
+  // Mapping to see all the Object and return each one to the Component(messageComponent)
+    const messageComponent = onScreenMessage.map((message) => {
+      return(
+        <div>
+          <p>{message.user}</p>
+          <p>{message.message}</p>
+        </div>
+      );
+    });
+
+  
 
 // #####    HTML    #####
   return (
     <div className="App">
       <div className='container'>
+        <div>
+          {messageComponent}
+        </div>
         <input
           value={valueInputUser}
           onChange={onChangeInputUserValue}
+          placeholder={"User..."}
         />
         <input
           value={valueInputMessage}
           onChange={onChangeInputMessageValue}
+          placeholder={"Message..."}
         />
-        <button onClick={onClickButton}>
+        <button onClick={onClickSendMessage}>
           Enviar
         </button>
       </div>
