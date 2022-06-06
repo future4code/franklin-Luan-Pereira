8b5a2ff31a64188e805ba5ec9271c88c27c7c64b
Luan Wallace <w.luan@yahoo.com>
2022-06-05 23:19:17 -0300

    Projeto 90%

--- modulo1/whatslab/src/App.js
+++ modulo1/whatslab/src/App.js
@@ -1,87 +1,114 @@
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import './App.css';
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+import sendIcon from "img/iconeSend.png"
 
 function App() {
+  const MsgBaloon = styled.div`
+    border-radius: 10px;
+    margin: 10px;
+    padding: 15px 40px 15px 15px;
+    background-color: #ffffff;
+    width: max-content;
+    box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.15);
+  `;
+
+  const MsgBaloonMe = styled.div`
+    border-radius: 10px;
+    margin: 10px 10px 10px auto;
+    padding: 15px 40px 15px 15px;
+    background-color: #ffffff;
+    width: max-content;
+    box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.15);
+  `;
+
+  const MsgBaloonName = styled.span`
+    color: #7a5c61;
+    font-weight: bold;
+    font-size: 14px;
+    text-transform: capitalize;
+  `;
+
+  const MsgBaloonText = styled.span`
+    word-wrap: wrap;
+    max-width: 400px;
+    display: block;
+    font-size: 18px;
+  `;
+
   // state do input
-  const [valueInputMessage, setvalueInputMessage] = useState('')
-  const [valueInputUser, setvalueInputUser] = useState('')
+  const [valueInputMessage, setvalueInputMessage] = useState("");
+  const [valueInputUser, setvalueInputUser] = useState("");
 
-  const [onScreenMessage, setonScreenMessage] = useState(
-  [ 
-    {
-        // User: "",
-        // Message: "",
-    },                                          // That's an Array of Objects
-    {
-        // User: "",
-        // Message: "",
-    } 
-  ]
-);
-
+  const [onScreenMessage, setonScreenMessage] = useState([]); // That's an Array of Objects
 
   // onChage USER and MESSAGE
   const onChangeInputUserValue = (element) => {
-
-    setvalueInputUser(element.target.value)
-  }
+    setvalueInputUser(element.target.value);
+  };
 
   const onChangeInputMessageValue = (element) => {
-    
-    setvalueInputMessage(element.target.value)
-  }
+    setvalueInputMessage(element.target.value);
+  };
 
-       // onClick Button
+  // onClick Button
   const onClickSendMessage = () => {
     const newMessage = {
-      user: valueInputUser + ":",
+      user: valueInputUser,
       message: valueInputMessage,
     };
 
-       // We have to copy the array to avoid bug
-    const messageCopy = [...onScreenMessage,newMessage];
+    // We have to copy the array to avoid bug
+    const messageCopy = [...onScreenMessage, newMessage];
 
     setonScreenMessage(messageCopy);
-      
-      // Clear Input
+
+    // Clear Input
     setvalueInputUser("");
     setvalueInputMessage("");
-  }
+  };
 
   // Mapping to see all the Object and return each one to the Component(messageComponent)
-    const messageComponent = onScreenMessage.map((message) => {
-      return(
-        <div>
-          <p>{message.user}</p>
-          <p>{message.message}</p>
-        </div>
+  const messageComponent = onScreenMessage.map((message) => {
+    if (message.user.toLowerCase() === "eu") {
+      return (
+        <MsgBaloonMe>
+          <MsgBaloonName>Você</MsgBaloonName>
+          <MsgBaloonText>{message.message}</MsgBaloonText>
+        </MsgBaloonMe>
       );
-    });
+    } else {
+      return (
+        <MsgBaloon>
+          <MsgBaloonName>{message.user}</MsgBaloonName>
+          <MsgBaloonText>{message.message}</MsgBaloonText>
+        </MsgBaloon>
+      );
+    }
+  });
 
-  
-
-// #####    HTML    #####
+  // #####    HTML    #####
   return (
     <div className="App">
-      <div className='container'>
-        <div>
-          {messageComponent}
-        </div>
-        <input
-          value={valueInputUser}
-          onChange={onChangeInputUserValue}
-          placeholder={"User..."}
-        />
-        <input
-          value={valueInputMessage}
-          onChange={onChangeInputMessageValue}
-          placeholder={"Message..."}
-        />
-        <button onClick={onClickSendMessage}>
-          Enviar
-        </button>
+      <div className="container">
+        <div>{messageComponent}</div>
+        <section className="interacaoUser">
+          <input
+            className="inputUser"
+            value={valueInputUser}
+            onChange={onChangeInputUserValue}
+            placeholder={"Usuário"}
+          />
+          <input
+            className="inputMessage"
+            value={valueInputMessage}
+            onChange={onChangeInputMessageValue}
+            placeholder={"Mensagem"}
+          />
+          <button className="btnForward" onClick={onClickSendMessage}>
+            {sendIcon}
+          </button>
+        </section>
       </div>
     </div>
   );
