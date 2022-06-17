d1f5b3ea7ff344b1726900a3088132a92ca723a8
Luan Wallace <w.luan@yahoo.com>
2022-06-17 13:23:40 -0300

    layout pronto

---
+++ modulo1/apis-labenusers/api-labenusers/src/components/CadastroUsuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CadastroUsuario = () => {
+    return (
+        <div>
+            <input placeholder="Nome" />
+            <input placeholder="E-mail" />
+            <button>Criar Usuario</button>
+        </div>
+    );
+}
+
+export default CadastroUsuario
---
+++ modulo1/apis-labenusers/api-labenusers/src/components/ListaUsuario.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ListaUsuario = () => {
+    return (
+        <div>
+            <div> </div>
+            <div>
+                <h3>Procurar Usuario</h3>
+                <input placeholder="Nome exato para busca" />
+                <button>Salvar edição</button>
+            </div>
+        </div>
+    );
+}
+
+export default ListaUsuario
