bfc383647d2b0451c563913b021f2ab09c964f8d
Luan Wallace <w.luan@yahoo.com>
2022-06-27 00:46:57 -0300

    [FEAT] rotas criadas na pasta Router

---
+++ modulo1/projeto-labex/labex/src/routes/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import MyTripDetailsPage from "../pages/MyTripDetailsPage";
+import MyTripsPage from '../pages/MyTripsPage';
+import NewTripPage from "../pages/NewTripPage";
+import OurTripDetailsPage from "../pages/OurTripDetailsPage";
+import OurTripPage from '../pages/OurTripPage';
+import SignUpPage from "../pages/SignUpPage";
+import ErrorPage from "../pages/ErrorPage";
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage/>} />
+          <Route path="login/" element={<LoginPage/>} />
+          <Route path="mytripdetails/" element={<MyTripDetailsPage/>} />
+          <Route path="mytrips/" element={<MyTripsPage/>} />
+          <Route path="newtrip/" element={<NewTripPage/>} />
+          <Route path="ourtripdetails/" element={<OurTripDetailsPage/>} />
+          <Route path="ourtrip/" element={<OurTripPage/>} />
+          <Route path="signup/" element={<SignUpPage/>} />
+          <Route path="*" element={<ErrorPage/>} />
+        </Routes>
+      </BrowserRouter>
+    );
+}
+
+export default Router;
