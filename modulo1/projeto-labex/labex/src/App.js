65bf8c7114b305a287af4577a47362287d39e0d9
Luan Wallace <w.luan@yahoo.com>
2022-06-27 00:32:43 -0300

    [FEAT] rotas criadas

--- modulo1/projeto-labex/labex/src/App.js
+++ modulo1/projeto-labex/labex/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import HomePage from "./pages/HomePage"
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import MyTripDetailsPage from "./pages/MyTripDetailsPage";
+import MyTripsPage from './pages/MyTripsPage';
+import NewTripPage from "./pages/NewTripPage";
+import OurTripDetailsPage from "./pages/OurTripDetailsPage";
+import OurTripPage from './pages/OurTripPage';
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div>
-        <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage/>} />
+          <Route path="login/" element={<LoginPage/>} />
+          <Route path="mytripdetails/" element={<MyTripDetailsPage/>} />
+          <Route path="mytrips/" element={<MyTripsPage/>} />
+          <Route path="newtrip/" element={<NewTripPage/>} />
+          <Route path="ourtripdetails/" element={<OurTripDetailsPage/>} />
+          <Route path="ourtrip/" element={<OurTripPage/>} />
+          <Route path="signup/" element={<SignUpPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
