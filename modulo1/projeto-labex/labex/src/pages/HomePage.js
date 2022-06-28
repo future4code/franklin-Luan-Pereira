847e3b4c4c0b55b44e2f1a74ab4b47d8f2a07fe1
Luan Wallace <w.luan@yahoo.com>
2022-06-27 23:52:10 -0300

    [FEAT] caminho de botoes

--- modulo1/projeto-labex/labex/src/pages/HomePage.js
+++ modulo1/projeto-labex/labex/src/pages/HomePage.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom'            //permite acessar o historico dentro de qualquer componente fuuncional
+
+
 
 function HomePage() {
+  const navigate = useNavigate();              // navigate permite a trocar a pagina atual
+
+  const goToLoginPage = () => {                // funcao que utiliza o navigate para ir para outra pagina
+    navigate("login/")                        // podemos chamar direto com navigate("login/")
+  }
+
+  
+
   return (
     <div >
         <img />
@@ -8,7 +19,7 @@ function HomePage() {
             <h1>Seja Bem Vindo ao Labex</h1>
             <div className='buttons'>
                 <button>Ver Viagens</button>
-                <button>Login</button>
+                <button onClick={() => { navigate("login/")} }>Login</button>                {/* vai para pagina de Login*/}
             </div>
         </div>
     </div>
