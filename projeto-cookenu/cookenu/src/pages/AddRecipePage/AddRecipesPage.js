3fcd256ca6836da3f3fc84c8a27aaf1dd0155f39
Luan Wallace <w.luan@yahoo.com>
2022-07-03 17:58:35 -0300

    [FEAT] Add Recipe done

--- projeto-cookenu/cookenu/src/pages/AddRecipePage/AddRecipesPage.js
+++ projeto-cookenu/cookenu/src/pages/AddRecipePage/AddRecipesPage.js
@@ -2,18 +2,67 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useProtectedPage } from "../../hooks/useProtectedPage"
+import useForm from "../../hooks/useForm";
+import {BASE_URL} from "../../constants/urls"
 
 
 function AddRecipesPage() {
     useProtectedPage()
 
+    const [form, onChange, clear] = useForm({title: "", description: "",picture: ""})
+    console.log(form)
+
+    const url = `${BASE_URL}/recipe`
+
+    const createRecipe = () => {
+        axios
+            .post(url, form,{      // form = body
+                headers: {
+                    Authorization: localStorage.getItem("token")
+                }
+            })
+            .then((response) =>{
+                alert(response.data.message)
+                clear()
+            })
+            .catch((error) => {
+                alert(error.responde.message)
+            })
+
+    }
+
+
     return(
         <div>
             <h1> Adicionar Nova Receita </h1>
-            <input placeholder="titulo" />
-            <input placeholder="descricao" />
-            <input placeholder="foto" />
-            <button>Adicionar</button>
+            <p> titulo </p>
+            <input
+                    name={"title"}
+                    value={form.title}
+                    onChange={onChange}
+                    label={"titulo"}
+                    type="text"
+                    required
+            />
+            <p> descrição</p>
+            <input
+                    name={"description"}
+                    value={form.description}
+                    onChange={onChange}
+                    label={"titulo"}
+                    type="text"
+                    required
+            />
+            <p> imagem </p>
+            <input
+                    name={"picture"}
+                    value={form.picture}
+                    onChange={onChange}
+                    label={"titulo"}
+                    type="text"
+                    required
+            />
+            <button onClick={createRecipe}>Adicionar</button>
         </div>
 
     );
